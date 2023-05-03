ee601918a6533effab92bd1ba705d2a9fb619afe
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-03 19:21:45 -0300

    docs: add jsdocs to CnpjValidator

--- src/cnpj-validator.ts
+++ src/cnpj-validator.ts
@@ -1,4 +1,9 @@
 export class CnpjValidator {
+    /**
+     * Validates a cnpj.
+     * @param {string} cnpj
+     * @returns {boolean}
+    */
     static validate(cnpj: string): boolean {
         const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
         return cnpjRegex.test(cnpj)
